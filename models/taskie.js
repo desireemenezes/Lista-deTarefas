00fa208a7bd73a77037739700beb024fc56b9ad1
Desirée Menezes <deemenezes@hotmail.com>
2018-06-02 11:23:23 -0300

    update taskie

--- models/taskie.js
+++ models/taskie.js
@@ -1,7 +1,2 @@
-/*O ARQUIVO É CLIENTE  E SRVIDOR
- CRIA UMA COLLECTION NO BANCO
-
-Tarefas com "T " maiusculo nome da variavel no js.
-com "t" minusculo nome tabela no banco*/
 
 Tarefas = new Mongo.Collection('tarefas');
